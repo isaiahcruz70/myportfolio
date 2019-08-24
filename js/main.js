81b6801d12980603c9a67a3de4a2a1a4815e57e1
Isaiah cruz <isaiahcruz70@gmail.com>
2019-08-24 12:23:07 -0700

    fixed links to projects

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// var userEnterSpeed: = parseInt(prompt)
+
+// class Animal {
+//     constructor(name,speed) {
+//     this.name = name;
+//     this.speed = 25;
+//     this.point = 0;
+// }
+
+// increaseSpeed() {
+//     this.speed  += "20";
+// }
+
+// gainPoints(pointValue) {
+//     if (isNaN(pointValue)) {
+
+//     } else {
+//         this.point += pointValue;
+//     }
+// }
+
+// alert (hello)
